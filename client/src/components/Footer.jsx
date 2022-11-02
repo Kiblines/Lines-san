99e02174c226af33b2573b69f3697e6b251dcdc8
Anthony Lines <linesanthony018@gmail.com>
2022-11-02 18:53:08 +0100

    footer ok pas de link encore

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,32 @@
 import React from "react";
 import styled from "styled-components";
+import facebook from "../fb.png";
+import youtube from "../youtube.png";
+import twitter from "../twitter.png";
+import instagram from "../instagram.png";
+import logo from "../banderole.jpeg";
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background-image: url(${logo});
+`;
+
+const StyledIcon = styled.img`
+  height: 80px;
+`;
 
 export default function Footer() {
-  return <div>Lines-san tout droit reservé</div>;
+  return (
+    <>
+      <StyledDiv>
+        <StyledIcon src={facebook}></StyledIcon>
+        <StyledIcon style={{ height: 70 }} src={youtube}></StyledIcon>
+        <StyledIcon src={twitter}></StyledIcon>
+        <StyledIcon src={instagram}></StyledIcon>
+      </StyledDiv>
+    </>
+  );
 }
