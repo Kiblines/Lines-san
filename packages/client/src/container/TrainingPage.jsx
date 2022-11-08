e254f2155e1e6bd6fef0bd68b68648d5d3aed35a
Anthony Lines <linesanthony018@gmail.com>
2022-11-08 22:53:55 +0100

    form

--- packages/client/src/container/TrainingPage.jsx
+++ packages/client/src/container/TrainingPage.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
 function TrainingPage() {
-  return <div>TrainingPage</div>;
+  return (
+    <div>
+      Dans cet article, nous allons parler du Katakana（片仮名/カタカナ）, la
+      deuxième famille d’“alphabets” de la langue japonaise. Au menu Le Katakana
+      en japonais Le japonais est une langue qui fonctionne par “syllabes”. Tout
+      comme le Hiragana que nous avons déjà introduit dans un article précédent,
+      le Katakana est aussi un alphabet qui permet de traduire des syllabes
+      phonétiques en lettres. Mais pourquoi s’embêter avec un second système
+      d’écriture alors qu’on a déjà les Hiragana? Eh bien en fait, les Katakanas
+      sont principalement utilisés pour écrire des mots importés de l’étranger,
+      comme de l’anglais, du français ou parfois même du chinois (un mot qui
+      existe déjà en Kanji mais dans une prononciation chinoise par exemple). Du
+      moins, c’est ce que vous raconteront la majorité les professeurs de
+      japonais. Mais il existe également trois autres grandes utilisations des
+      Katakanas et ce, crucial dans la lecture des Mangas: Pour remplacer des
+      mots normalement écrits en Kanji, dans le cas où l’écrivain aurait la
+      flemme de les écrire, ou alors pour les mettre en valeur dans une phrase,
+      comme par exemple les insultes (ex:「バカ（baka）= con」à la place
+      de「馬鹿（baka）」en Kanji, ou 「ヤロー（yaro）= gars」 à la place de
+      「野郎（yarou）」) Pour les noms des personnages, aussi bien dans le cas
+      où l’écrivain aurait eu la flemme de réfléchir à des Kanji, ou alors dans
+      le cas où aucun Kanji n’existerait pour ces noms (ex: la majorité des noms
+      des personnages de Dragon Ball, One Piece, etc. et tous les noms de
+      Pokémons). Quelques fois l’auteur fait même exprès de les écrire en
+      Katakana pour les mettre en relief. Pour la plupart des onomatopées,
+      surtout dans les mangas.
+    </div>
+  );
 }
 
 export default TrainingPage;
