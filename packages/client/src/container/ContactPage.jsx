381a872f7c93e471dc418b3ab40011227c46d9e3
Anthony Lines <linesanthony018@gmail.com>
2022-11-09 14:57:50 +0100

    commit de la heiss

--- packages/client/src/container/ContactPage.jsx
+++ packages/client/src/container/ContactPage.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import Form from "../components/Form";
 
 export default function ContactPage() {
-  return <div>ContactPage</div>;
+  return (
+    <div>
+      ContactPage
+      <Form />
+    </div>
+  );
 }
